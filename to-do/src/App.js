be5738e5f731c5404c3ead414d5cfefd655caf46
Franklin Antony <franklinanton.007@gmail.com>
2018-01-18 00:26:35 +0530

    Edited App.js

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -3,8 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  
-  
+  constructor(){
+      super();
+      this.state ={
+          count: 0
+      };
+  }
+  render(){
+    return(
+        <div>
+        <button>Increment</button>
+        {this.state.count}
+        </div>
+    );
+
+  }
 }
 
 export default App;
