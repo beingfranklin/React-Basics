9aabf3d34624931a875efcbf1304a15eeaa64c6b
Franklin Antony <franklinanton.007@gmail.com>
2019-04-29 14:30:05 +0530

    added dashboard

---
+++ SignIn-SignUp-Form/src/Router.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+
+
+const Router = () => (
+
+<BrowserRouter>
+<Switch>
+    <Route exact path="/dashboard" component={Dashboard}/>
+{/* <Route> */}
+    {/* <Route> */}
+</Switch>
+</BrowserRouter>
+
+
+
+);
+
+
+export default Router;
