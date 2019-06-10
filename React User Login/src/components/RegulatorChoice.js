b6263d0d10007ef9aad23fc486efcaffc972abaa
Franklin Antony <franklinanton.007@gmail.com>
2019-06-11 01:07:12 +0530

    Added card and buttons mockup

--- React User Login/src/components/RegulatorChoice.js
+++ React User Login/src/components/RegulatorChoice.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import Card from 'card-vibes';
+
 class RegulatorChoice extends Component {
+   
+
     render() {
          return (
       <div className="App" >
            <div className="box-header with-border">
-                                    <h3 className="box-title">Hey, Regulator! Select Your Choice!!</h3>
-                                    {/* Choices - Create Doctor and create hospital */}
+                                   
+                              
+                                    <Card style={{ width: '100%', padding: '20px' }}>
+                                          <h3 className="box-title">Hey, Regulator! Select Your Choice!!</h3>
+                                    </Card>
+                                         <button class="FormField__Button mr-20" >Create Doctor</button>
+                                         <button class="FormField__Button mr-20" >Create Hospital</button>
+
                                 </div>
           </div>
           );
